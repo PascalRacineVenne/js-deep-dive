6634427f3df09cc6da78b1cb256da74dd4472780
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-08-29 23:38:34 -0400

    first part on var and string

---
+++ var-string/index.js
@@ -0,0 +1,16 @@
+var username;
+
+// returns undefined
+
+var username = 'pascal';
+console.log(username);
+
+// camelCased
+
+var firstName = 'Mayline';
+
+// name only contains letters, numbers, $ _
+// first charact must not be a number
+
+// word that can't be variables
+// reserved words
